19572fe4ebebca1f79a3d06050b6265bc22e1b0a
Anthony Scinocco <scinocco.a@gmail.com>
2017-06-18 16:58:49 -0400

    updated routes and route the store method

--- routes/core/auth.routes.js
+++ routes/core/auth.routes.js
@@ -3,21 +3,21 @@ let AuthController = require('../../controllers/core/auth.controller.js');
 module.exports = [
     {
         method: 'POST',
-        path: '/sign-up',
+        path: '/api/sign-up',
         config: { auth: false },
         handler: AuthController.signUp
     },
 
     {
         method: 'POST',
-        path: '/sign-in',
+        path: '/api/sign-in',
         config: { auth: false },
         handler: AuthController.signIn
     },
 
     {
         method: 'POST',
-        path: '/sign-out',
+        path: '/api/sign-out',
         config: { auth: 'jwt' },
         handler: AuthController.signOut
     }
